06faaf83d7b19ccbe02c728d8135e7d4a03325be
zahra <zahra.alsabah96@gmail.com>
2021-04-04 18:23:13 +0100

    added create user and authentication

--- company-portal-server/index.js
+++ company-portal-server/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const users = require("./routes/users");
+const auth = require("./routes/auth");
+const  mongoose  = require("mongoose");
 const app = express();
+
+mongoose.connect('mongodb://localhost/portal', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+.then(() => console.log("connected to DB"))
+app.use(express.json())
 
 app.get("/", (req, res) =>{
     res.send("test");
 })
+app.use('/api/users', users)
+app.use('/api/auth', auth)
 
 app.listen(3001, () =>{
     console.log("running");
